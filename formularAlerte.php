2925e6e1435bc51945d8c890d1048ac46767fc5a
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2019-05-01 00:12:48 +0300

    form

---
+++ formularAlerte.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <link href="formularAlerte.css" rel="stylesheet" type="text/css">
+</head>
+    
+<body>
+    
+    <div id="formular">
+        <h3>Alert Data</h3>
+        <form>
+            
+                 <label for="tipalerta">Emergency Type:</label>
+            <input type="text" name="alerttype" id="tipalerta" value="<?php echo $_GET["tipalerta"];?>"; />
+         
+            
+            <label for="location" >Location:</label>
+            <input type="text" name="location" placeholder="Complete if the emergency is not in your area!"/>
+             <label for="areaDesc">Details:</label>
+            <textarea id="detalii" name="areaDesc"> </textarea>
+            <label for="sender">Name:</label>
+            <input type="text" name="sender">
+            <br>
+            <input type="submit" name="send" value="SEND"/>
+            
+        <br>
+        
+        </form>
+    
+    
+    </div>
+   
+
+    
+</body>
+
+</html>
