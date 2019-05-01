5b58d2dcaebb15693d9300d5ecce60051fdff85a
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2019-05-01 12:11:53 +0300

    Revert "stilizare form"
    This reverts commit 20c84e5a011d0964b51522d987c4726a71d6fd73.

--- formularAlerte.php
+++ formularAlerte.php
@@ -7,7 +7,7 @@
 <body>
     
     <div id="formular">
-        <h1>Alert Data</h1>
+        <h3>Alert Data</h3>
         <form>
             
                  <label for="tipalerta">Emergency Type:</label>
@@ -18,10 +18,9 @@
             <input type="text" name="location" placeholder="Complete if the emergency is not in your area!"/>
              <label for="areaDesc">Details:</label>
             <textarea id="detalii" name="areaDesc"> </textarea>
-            <label for="sender">Your Name:</label>
+            <label for="sender">Name:</label>
             <input type="text" name="sender">
             <br>
-            
             <input type="submit" name="send" value="SEND"/>
             
         <br>
